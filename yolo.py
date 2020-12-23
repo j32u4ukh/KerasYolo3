d7eed372efb2ea370f03090c4734a7e4076cf852
j32u4ukh <j32u4ukh@gmail.com>
2020-12-23 22:00:18 +0800

    detectImage 不使用 assert 改為直接修正 size

--- yolo.py
+++ yolo.py
@@ -8,25 +8,25 @@ import os
 from timeit import default_timer as timer
 
 import numpy as np
-from keras import backend as K
-from keras.models import load_model
-from keras.layers import Input
 from PIL import Image, ImageFont, ImageDraw
+from keras import backend as K
+from keras.layers import Input
+from keras.models import load_model
+from keras.utils import multi_gpu_model
 
 from yolo3.model import yolo_eval, yolo_body, tiny_yolo_body
 from yolo3.utils import letterbox_image
-import os
-from keras.utils import multi_gpu_model
 
-class YOLO(object):
+
+class YOLO:
     _defaults = {
         "model_path": 'model_data/yolo.h5',
         "anchors_path": 'model_data/yolo_anchors.txt',
         "classes_path": 'model_data/coco_classes.txt',
-        "score" : 0.3,
-        "iou" : 0.45,
-        "model_image_size" : (416, 416),
-        "gpu_num" : 1,
+        "score": 0.3,
+        "iou": 0.45,
+        "model_image_size": (416, 416),
+        "gpu_num": 1,
     }
 
     @classmethod
@@ -37,12 +37,15 @@ class YOLO(object):
             return "Unrecognized attribute name '" + n + "'"
 
     def __init__(self, **kwargs):
-        self.__dict__.update(self._defaults) # set up default values
-        self.__dict__.update(kwargs) # and update with user overrides
+        self.__dict__.update(self._defaults)  # set up default values
+        self.__dict__.update(kwargs)  # and update with user overrides
         self.class_names = self._get_class()
         self.anchors = self._get_anchors()
         self.sess = K.get_session()
         self.boxes, self.scores, self.classes = self.generate()
+        # self.colors = None
+        # self.input_image_shape = None
+        # self.yolo_model = None
 
     def _get_class(self):
         classes_path = os.path.expanduser(self.classes_path)
@@ -65,17 +68,17 @@ class YOLO(object):
         # Load model, or construct model and load weights.
         num_anchors = len(self.anchors)
         num_classes = len(self.class_names)
-        is_tiny_version = num_anchors==6 # default setting
+        is_tiny_version = num_anchors == 6  # default setting
         try:
             self.yolo_model = load_model(model_path, compile=False)
         except:
-            self.yolo_model = tiny_yolo_body(Input(shape=(None,None,3)), num_anchors//2, num_classes) \
-                if is_tiny_version else yolo_body(Input(shape=(None,None,3)), num_anchors//3, num_classes)
-            self.yolo_model.load_weights(self.model_path) # make sure model, anchors and classes match
+            self.yolo_model = tiny_yolo_body(Input(shape=(None, None, 3)), num_anchors // 2, num_classes) \
+                if is_tiny_version else yolo_body(Input(shape=(None, None, 3)), num_anchors // 3, num_classes)
+            self.yolo_model.load_weights(self.model_path)  # make sure model, anchors and classes match
         else:
-            assert self.yolo_model.layers[-1].output_shape[-1] == \
-                num_anchors/len(self.yolo_model.output) * (num_classes + 5), \
-                'Mismatch between model and given anchor and class sizes'
+            assert (self.yolo_model.layers[-1].output_shape[-1] ==
+                    num_anchors / len(self.yolo_model.output) * (num_classes + 5),
+                    'Mismatch between model and given anchor and class sizes')
 
         print('{} model, anchors, and classes loaded.'.format(model_path))
 
@@ -91,29 +94,37 @@ class YOLO(object):
         np.random.seed(None)  # Reset seed to default.
 
         # Generate output tensor targets for filtered bounding boxes.
-        self.input_image_shape = K.placeholder(shape=(2, ))
-        if self.gpu_num>=2:
+        self.input_image_shape = K.placeholder(shape=(2,))
+
+        if self.gpu_num >= 2:
             self.yolo_model = multi_gpu_model(self.yolo_model, gpus=self.gpu_num)
+
         boxes, scores, classes = yolo_eval(self.yolo_model.output, self.anchors,
-                len(self.class_names), self.input_image_shape,
-                score_threshold=self.score, iou_threshold=self.iou)
+                                           len(self.class_names), self.input_image_shape,
+                                           score_threshold=self.score, iou_threshold=self.iou)
         return boxes, scores, classes
 
     def detect_image(self, image):
+        # 用於計時
         start = timer()
 
+        # 確保圖片長寬為 32 的倍數
         if self.model_image_size != (None, None):
-            assert self.model_image_size[0]%32 == 0, 'Multiples of 32 required'
-            assert self.model_image_size[1]%32 == 0, 'Multiples of 32 required'
+            assert self.model_image_size[0] % 32 == 0, 'Multiples of 32 required'
+            assert self.model_image_size[1] % 32 == 0, 'Multiples of 32 required'
             boxed_image = letterbox_image(image, tuple(reversed(self.model_image_size)))
         else:
-            new_image_size = (image.width - (image.width % 32),
-                              image.height - (image.height % 32))
+            new_image_size = (image.width - (image.width % 32), image.height - (image.height % 32))
             boxed_image = letterbox_image(image, new_image_size)
+
         image_data = np.array(boxed_image, dtype='float32')
 
         print(image_data.shape)
+
+        # 數值正規化
         image_data /= 255.
+
+        # 擴增一個維度，在 axis = 0
         image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
 
         out_boxes, out_scores, out_classes = self.sess.run(
@@ -126,16 +137,26 @@ class YOLO(object):
 
         print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
 
+        # 設定輸出文字
         font = ImageFont.truetype(font='font/FiraMono-Medium.otf',
-                    size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
+                                  size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
+
+        # 設定框的粗細
         thickness = (image.size[0] + image.size[1]) // 300
 
         for i, c in reversed(list(enumerate(out_classes))):
+            # 預測類別
             predicted_class = self.class_names[c]
+
+            # 預測框
             box = out_boxes[i]
+
+            # 機率/信心
             score = out_scores[i]
 
             label = '{} {:.2f}'.format(predicted_class, score)
+
+            # 載入圖片
             draw = ImageDraw.Draw(image)
             label_size = draw.textsize(label, font)
 
@@ -146,38 +167,142 @@ class YOLO(object):
             right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
             print(label, (left, top), (right, bottom))
 
+            # 定位文字位置
             if top - label_size[1] >= 0:
                 text_origin = np.array([left, top - label_size[1]])
             else:
                 text_origin = np.array([left, top + 1])
 
             # My kingdom for a good redistributable image drawing library.
-            for i in range(thickness):
-                draw.rectangle(
-                    [left + i, top + i, right - i, bottom - i],
-                    outline=self.colors[c])
-            draw.rectangle(
-                [tuple(text_origin), tuple(text_origin + label_size)],
-                fill=self.colors[c])
+            for j in range(thickness):
+                draw.rectangle([left + j, top + j, right - j, bottom - j],
+                               outline=self.colors[c])
+
+            # 文字標籤矩形(顏色填滿)
+            draw.rectangle([tuple(text_origin), tuple(text_origin + label_size)],
+                           fill=self.colors[c])
+
+            # 寫上文字
             draw.text(text_origin, label, fill=(0, 0, 0), font=font)
+
+            # 刪除圖片
             del draw
 
-        end = timer()
-        print(end - start)
+        # 印出總共花費時間
+        print(timer() - start)
+
+        return image
+
+    def detectImage(self, image):
+        # 用於計時
+        start = timer()
+
+        # 確保圖片長寬為 32 的倍數
+        if self.model_image_size != (None, None):
+            # 將 assert 改為直接修正 size 要求，避免 size 出錯時程式直接報錯停止
+            # assert self.model_image_size[0] % 32 == 0, 'Multiples of 32 required'
+            # assert self.model_image_size[1] % 32 == 0, 'Multiples of 32 required'
+            self.model_image_size[0] = self.model_image_size[0] - self.model_image_size[0] % 32
+            self.model_image_size[1] = self.model_image_size[1] - self.model_image_size[1] % 32
+
+            # 將圖片縮放成指定大小
+            boxed_image = letterbox_image(image, tuple(reversed(self.model_image_size)))
+        else:
+            # 若 self.model_image_size 未定義
+            new_image_size = (image.width - (image.width % 32), image.height - (image.height % 32))
+
+            # 將圖片縮放成指定大小
+            boxed_image = letterbox_image(image, new_image_size)
+
+        image_data = np.array(boxed_image, dtype='float32')
+
+        print(image_data.shape)
+
+        # 數值正規化
+        image_data /= 255.
+
+        # 擴增一個維度，在 axis = 0
+        image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
+
+        out_boxes, out_scores, out_classes = self.sess.run(
+            [self.boxes, self.scores, self.classes],
+            feed_dict={
+                self.yolo_model.input: image_data,
+                self.input_image_shape: [image.size[1], image.size[0]],
+                K.learning_phase(): 0
+            })
+
+        print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
+
+        # 設定輸出文字
+        font = ImageFont.truetype(font='font/FiraMono-Medium.otf',
+                                  size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
+
+        # 設定框的粗細
+        thickness = (image.size[0] + image.size[1]) // 300
+
+        for i, c in reversed(list(enumerate(out_classes))):
+            # 預測類別
+            predicted_class = self.class_names[c]
+
+            # 預測框
+            box = out_boxes[i]
+
+            # 機率/信心
+            score = out_scores[i]
+
+            label = '{} {:.2f}'.format(predicted_class, score)
+
+            # 載入圖片
+            draw = ImageDraw.Draw(image)
+            label_size = draw.textsize(label, font)
+
+            top, left, bottom, right = box
+            top = max(0, np.floor(top + 0.5).astype('int32'))
+            left = max(0, np.floor(left + 0.5).astype('int32'))
+            bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
+            right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
+            print(label, (left, top), (right, bottom))
+
+            # 定位文字位置
+            if top - label_size[1] >= 0:
+                text_origin = np.array([left, top - label_size[1]])
+            else:
+                text_origin = np.array([left, top + 1])
+
+            # My kingdom for a good redistributable image drawing library.
+            for j in range(thickness):
+                draw.rectangle([left + j, top + j, right - j, bottom - j],
+                               outline=self.colors[c])
+
+            # 文字標籤矩形(顏色填滿)
+            draw.rectangle([tuple(text_origin), tuple(text_origin + label_size)],
+                           fill=self.colors[c])
+
+            # 寫上文字
+            draw.text(text_origin, label, fill=(0, 0, 0), font=font)
+
+            # 刪除圖片
+            del draw
+
+        # 印出總共花費時間
+        print(timer() - start)
+
         return image
 
     def close_session(self):
         self.sess.close()
+
 
 def detect_video(yolo, video_path, output_path=""):
     import cv2
     vid = cv2.VideoCapture(video_path)
     if not vid.isOpened():
         raise IOError("Couldn't open webcam or video")
-    video_FourCC    = int(vid.get(cv2.CAP_PROP_FOURCC))
-    video_fps       = vid.get(cv2.CAP_PROP_FPS)
-    video_size      = (int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)),
-                        int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    video_FourCC = int(vid.get(cv2.CAP_PROP_FOURCC))
+    video_fps = vid.get(cv2.CAP_PROP_FPS)
+    video_size = (int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                  int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT)))
     isOutput = True if output_path != "" else False
     if isOutput:
         print("!!! TYPE:", type(output_path), type(video_FourCC), type(video_fps), type(video_size))
@@ -209,4 +334,3 @@ def detect_video(yolo, video_path, output_path=""):
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     yolo.close_session()
-
